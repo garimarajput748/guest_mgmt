0299d96d4909582c44f355a859a3dfd223cbee89
Garima Rajput <garimarajput748@gmail.com>
2022-11-15 19:36:26 +0530

    guest delete by ajax

--- guests/total-guests.php
+++ guests/total-guests.php
@@ -1,8 +1,24 @@
 <?php
 require_once("../path.php");
+require_once(SITE_ROOT_DIR_PATH . "dbConn/db.php");
+
+// query for delete data from database
+if (!empty($_GET['action']) && $_GET['action'] == 'delete_records' && !empty($_GET['deleteId'])) {
+  $deleteId = $_GET['deleteId'];
+  $sql = "DELETE FROM guest_list WHERE guest_id = $deleteId";
+
+  try {
+    $result = $conn->query($sql);
+    $message = "Record Delete Successfully";
+  } catch (Exception $e) {
+    $message = $e->getMessage();
+  }
+  echo json_encode(array("message" => $message));
+  exit;
+}
+
 require_once(SITE_ROOT_DIR_PATH . "include/header.php");
 require_once(SITE_ROOT_DIR_PATH . "include/sidebar.php");
-require_once(SITE_ROOT_DIR_PATH . "dbConn/db.php");
 
 // query for showing the data from database
 $guest_data = array();
@@ -18,25 +34,6 @@ if ($result->num_rows > 0) {
   $no_data =  "No Record Found :)";
 }
 
-// die(basename(__FILE__) . " : " . __LINE__);
-// echo '<br>MAYANK ' . basename(__FILE__) . ' ' . __LINE__ . '<pre> data :: ';
-// print_r($_GET);
-// echo '</pre>';
-// exit;
-// query for delete data from database
-if (!empty($_GET['action']) && $_GET['action'] == 'delete') {
-  if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "DELETE FROM guest_list WHERE guest_id = $id";
-    echo '<br>MAYANK ' . basename(__FILE__) . ' ' . __LINE__ . '<pre> data :: ';
-    print_r($sql);
-    echo '</pre>';
-    exit;
-    $result = $conn->query($sql);
-    if ($result) $mesg = "Record deleted successfully";
-    else $mesg_err =  "Error deleting record";
-  }
-}
 $conn->close();
 
 //  for send invitation to selected guest 
@@ -60,45 +57,45 @@ if (isset($_POST['invitation'])) {
         <a href="<?php echo BASE_URL; ?>guests/add-guest.php"><button type="submit" class="mb-3 add-btn"><i class="bi bi-plus"></i>Add New Guest</button></a>
         <div class="card rounded shadow border-0">
           <div class="card-body p-5 bg-white rounded">
-              <div class="table-responsive">
-                <table id="guest-table" style="width:100%" class="table table-striped table-bordered">
-                  <thead>
-                    <tr class="text-center">
-                      <th class="text-nowrap">Select All <input type="checkbox" name="chk-all" value="chk-all" onchange="checkAll(this)"></th>
-                      <th class="text-nowrap">Sr. No.</th>
-                      <th>Name</th>
-                      <th class="text-nowrap">Contact No.</th>
-                      <th>Address</th>
-                      <th>Relationship</th>
-                      <th>Edit / Delete</th>
+            <div class="table-responsive">
+              <table id="guest-table" style="width:100%" class="table table-striped table-bordered">
+                <thead>
+                  <tr class="text-center">
+                    <th class="text-nowrap">Select All <input type="checkbox" name="chk-all" value="chk-all" onchange="checkAll(this)"></th>
+                    <th class="text-nowrap">Sr. No.</th>
+                    <th>Name</th>
+                    <th class="text-nowrap">Contact No.</th>
+                    <th>Address</th>
+                    <th>Relationship</th>
+                    <th>Edit / Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($guest_data as $data) { ?>
+                    <tr class="text-center table-row">
+                      <td><input type="checkbox" name="check" onchange="checkChange()" value="<?php echo $data['guest_id']; ?>"></td>
+                      <td><?php echo $data['guest_id']; ?></td>
+                      <td><?php echo $data['guest_name']; ?></td>
+                      <td><?php echo $data['guest_mobile']; ?></td>
+                      <td><?php echo $data['guest_address']; ?></td>
+                      <td><?php echo $data['relationship']; ?></td>
+                      <td>
+                        <a href="<?php BASE_URL ?>update-guest.php?action=edit&id=<?php echo $data['guest_id'] ?>" title="edit this">
+                          <i class="bi bi-pencil-square edit"></i>
+                        </a>&nbsp;&nbsp;
+                        <button class="border-0 bg-transparent" title="delete this" class="delete-btn" onclick="deleteRow(this)" data-id="<?php echo $data['guest_id'] ?>">
+                          <i class="bi bi-trash delete"></i>
+                        </button>
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    <?php foreach ($guest_data as $data) { ?>
-                      <tr class="text-center table-row">
-                        <td><input type="checkbox" name="check" onchange="checkChange()" value="<?php echo $data['guest_id']; ?>"></td>
-                        <td><?php echo $data['guest_id']; ?></td>
-                        <td><?php echo $data['guest_name']; ?></td>
-                        <td><?php echo $data['guest_mobile']; ?></td>
-                        <td><?php echo $data['guest_address']; ?></td>
-                        <td><?php echo $data['relationship']; ?></td>
-                        <td>
-                          <a href="<?php BASE_URL ?>update-guest.php?action=edit&id=<?php echo $data['guest_id'] ?>" title="edit this">
-                            <i class="bi bi-pencil-square edit"></i>
-                          </a>&nbsp;&nbsp;
-                          <button class="border-0 bg-transparent" title="delete this" class="delete-btn" onclick="deleteRow(this)" data-id="<?php echo $data['guest_id'] ?>">
-                            <i class="bi bi-trash delete"></i>
-                          </button> 
-                        </td>
-                      </tr>
-                    <?php } ?>
-                  </tbody>
-                </table>
-                <div><button class="border-0 bg-danger text-white p-1" title="Delete All">Delete All</button></div>
-                <div class="float-end"><?php echo (isset($sendInvitation)) ? $sendInvitation : ''; ?></div>
-                <div class="text-center text-success"><?php if (isset($mesg)) echo $mesg; ?></div>
-                <div class="text-center text-danger"><?php if (isset($mesg_err)) echo $mesg_err; ?><?php if (isset($no_data)) echo $no_data; ?></div>
-              </div>
+                  <?php } ?>
+                </tbody>
+              </table>
+              <div><button class="border-0 bg-danger text-white p-1" title="Delete All">Delete All</button></div>
+              <div class="float-end"><?php echo (isset($sendInvitation)) ? $sendInvitation : ''; ?></div>
+              <div class="text-center text-success"><?php if (isset($mesg)) echo $mesg; ?></div>
+              <div class="text-center text-danger"><?php if (isset($mesg_err)) echo $mesg_err; ?><?php if (isset($no_data)) echo $no_data; ?></div>
+            </div>
           </div>
         </div>
       </div>
@@ -107,7 +104,6 @@ if (isset($_POST['invitation'])) {
 </main>
 <script>
   $(document).ready(function() {
-    
     var table = $('#guest-table').DataTable({
       dom: 'Bfrtip',
       buttons: [{
@@ -172,25 +168,29 @@ if (isset($_POST['invitation'])) {
       ]
 
     });
-
-    //   $('#guest-table').on('click','.delete-btn',function(){
-    //     // $(this).closest('tr').remove();
-    //     console.log(this);
-    // });
-
-    // $.ajax({
-    //   url: '',
-    //   type: 'GET',
-    //   data: {
-    //     id: 1,
-    //     "action": "delete",
-    //   },
-    //   success: function(response) {
-    //     // console.log(`%c data`, 'font-size:50px; color: red;');
-    //   }
-    // });
   });
-  
+
+  /**
+   * This function help to delete record.
+   */
+  function deleteRow(ele) {
+    var deleteId = $(ele).attr("data-id");
+    $(ele).parents("tr").remove();
+    $.ajax({
+      url: '',
+      type: 'GET',
+      data: {
+        "deleteId": deleteId,
+        "action": "delete_records"
+      },
+      success: function(response) {
+        if (response != "") {
+          var result = JSON.parse(response);
+          alert(result.message);
+        }
+      }
+    });
+  }
 </script>
 
 <?php
--- pages-logout.php
+++ pages-logout.php
@@ -1,7 +1,6 @@
 <?php
-require_once(__DIR__. "/path.php");
-require_once(SITE_ROOT_DIR_PATH . "/utilities.php");
+require_once(__DIR__ . "/path.php");
+require_once(CLASSES_DIR . "/utilities.php");
 session_start();
 session_destroy();
 utilities::showPage("pages-login.php");
-?>
